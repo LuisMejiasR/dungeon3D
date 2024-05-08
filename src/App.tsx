17808aaf063e7e78507612f21a8fb948368d3e8a
LuimeND <146760424+LuimeND@users.noreply.github.com>
2024-05-08 16:00:02 -0400

    feat: adding saved state for camera in json

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Gltf, ScrollControls, useScroll } from "@react-three/drei";
 import { getProject, val } from "@theatre/core";
+import flyThroughState from "./assets/state.json"
 import './App.css'
 
 import {
@@ -10,7 +11,7 @@ import {
 } from "@theatre/r3f";
 
 export default function App() {
-  const sheet = getProject("Fly Through").sheet("Scene");
+  const sheet = getProject("Fly Through", { state: flyThroughState }).sheet("Scene");
 
   return (
     <Canvas gl={{ preserveDrawingBuffer: true }}>
@@ -29,21 +30,32 @@ function Scene() {
 
   // our callback will run on every animation frame
   useFrame(() => {
-    // the length of our sequence
-    const sequenceLength = val(sheet.sequence.pointer.length);
-    // update the "position" of the playhead in the sequence, as a fraction of its whole length
-    sheet.sequence.position = scroll.offset * sequenceLength;
+    if (sheet !== undefined) {
+      // the length of our sequence
+      const sequenceLength = val(sheet.sequence.pointer.length);
+      // update the "position" of the playhead in the sequence, as a fraction of its whole length
+      sheet.sequence.position = scroll.offset * sequenceLength;
+    }
   });
 
-  const bgColor = "#84a4f4";
+  const bgColor = "#5b5c57";
 
   return (
     <>
       <color attach="background" args={[bgColor]} />
-      <fog attach="fog" color={bgColor} near={-4} far={10} />
-      <ambientLight intensity={0.5} />
+      <fog
+        attach="fog"
+        color={bgColor}
+        near={0}
+        far={25}
+      />
+      <ambientLight intensity={0.7} />
       <directionalLight position={[-5, 5, -5]} intensity={1.5} />
-      <Gltf src="/environment1.glb" castShadow receiveShadow />
+      <Gltf
+        src="/environment1.glb"
+        castShadow
+        receiveShadow
+      />
       <PerspectiveCamera
         theatreKey="Camera"
         makeDefault
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import studio from '@theatre/studio';
-import extension from '@theatre/r3f/dist/extension';
+// import studio from '@theatre/studio';
+// import extension from '@theatre/r3f/dist/extension';
 import './index.css'
 
-studio.extend(extension);
-studio.initialize();
+// studio.extend(extension);
+// studio.initialize();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
