9161dbe6625b1588cd3e29191d49a32922fb2351
LuimeND <146760424+LuimeND@users.noreply.github.com>
2024-05-16 11:41:32 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/dungeon3D/'
+  base: '/dungeon3D/',
+  plugins: [react()]
 })
